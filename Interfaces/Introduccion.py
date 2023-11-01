dba5128bfca880fa5841ca64047651b6ebdeb398
MitziC3 <A01731147@tec.mx>
2023-11-01 16:58:05 -0600

    Backend

--- Interfaces/Introduccion.py
+++ Interfaces/Introduccion.py
@@ -1,1 +1,12 @@
 print("Hola mundo, esta es mi primer interfaz")
+a=50
+b=56
+c=69
+
+def suma():
+    print(a+b+c)
+suma()
+
+def imprime():
+    print("Esta es la rama para desarrollar el backend")
+imprime()
