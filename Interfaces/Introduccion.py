43bcc62788351662b47a2053e8483a1bbce08289
MitziC3 <145406461+MitziC3@users.noreply.github.com>
2023-11-02 16:02:02 -0600

    creacion de frontend (#1)

--- Interfaces/Introduccion.py
+++ Interfaces/Introduccion.py
@@ -1,4 +1,6 @@
-print("Hola mundo, esta es mi primer interfaz")
+
+print("Hola mundo, esta es mi primer interfaz, disfrutalo")
+
 a=50
 b=56
 c=69
@@ -6,3 +8,9 @@ c=69
 def suma():
     print(a+b+c)
 suma()
+
+
+def mostrar():
+    print("Esta es la creación de la rama de frontend")
+mostrar()
+
