63ceaf9262d3d10ab68043045a37c4f34b1f1784
MitziC3 <A01731147@tec.mx>
2023-11-02 15:48:28 -0600

    creacion de frontend

--- Interfaces/Introduccion.py
+++ Interfaces/Introduccion.py
@@ -1,1 +1,12 @@
-print("Hola mundo, esta es mi primer interfaz")
+print("Hola mundo, esta es mi primer interfaz, disfrutalo")
+a=50
+b=56
+c=69
+
+def suma():
+    print(a+b+c)
+suma()
+
+def mostrar():
+    print("Esta es la creación de la rama de frontend")
+mostrar()
